1a663702816b39b5e6be2b56c85afd85e2f011d7
Luka7711 <35857649+Luka7711@users.noreply.github.com>
2021-08-31 15:09:17 -0500

    url change

--- src/apis/movies.js
+++ src/apis/movies.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 
 const addMovieToDb = axios.create({
-  baseURL: "http://localhost:9000/chicago-cinema",
+  baseURL: "https://movie-dbs.herokuapp.com/chicago-cinema",
   method: "post",
 });
 
 const retrieveMovies = axios.create({
-  baseURL: "http://localhost:9000/chicago-cinema",
+  baseURL: "https://movie-dbs.herokuapp.com/chicago-cinema",
   method: "get",
 });
 
 const getDetails = axios.create({
-  baseURL: "http://localhost:9000/chicago-cinema/movies",
+  baseURL: "https://movie-dbs.herokuapp.com/chicago-cinema/movies",
   method: "get",
 });
 
